a4db8d7b8c50fa55ac561fb3cb5b29d89ca97d2f
samiabat <samuelabatneh20@gmail.com>
2022-11-28 00:41:02 +0300

    add pages

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,17 +1,25 @@
 <template>
-  <v-app-bar color="primary" app>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <v-app-bar color="indigo darken-2" app>
+    <v-app-bar-nav-icon
+          dark
+          @click.stop="drawer = !drawer"
+        >
+      </v-app-bar-nav-icon>
     <v-toolbar-title>SAMUEL ABATNEH PORTFOLIO</v-toolbar-title>
     <v-spacer></v-spacer>
+
     <v-btn class="pr-5">
-      <v-text>Home</v-text>
+      <router-link to="/">Home</router-link>
     </v-btn>
+
+    <v-btn to="/about" class="pr-5">
+      About
+    </v-btn>
+
     <v-btn class="pr-5">
-      <v-text>Projects</v-text>
+      <v-text>Certeficates</v-text>
     </v-btn>
-    <v-btn class="pr-5">
-      <v-text>Certificates</v-text>
-    </v-btn>
+
     <v-btn class="pr-5">
       <v-text>About</v-text>
     </v-btn>
@@ -19,54 +27,26 @@
       <v-text>Contact</v-text>
     </v-btn>
   </v-app-bar>
-  <pre>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </pre>
-
+  <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+    >
+      HelloWorld
+          
+    </v-navigation-drawer>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'Home', icon: 'mdi-view-dashboard' },
+          { title: 'About', icon: 'mdi-forum' },
+        ],
+      }
+    },
+  }
+</script>
